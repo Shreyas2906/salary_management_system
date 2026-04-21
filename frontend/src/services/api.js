b342392799cf59aa6a1d431a2e08928519344f85
shreya <shreyashirke29@gmail.com>
2026-04-21 12:04:42 +0530

    fix: allow frontend CORS

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://salary-management-system-backend.onrender.com",
 });
 
 // ✅ Add this
